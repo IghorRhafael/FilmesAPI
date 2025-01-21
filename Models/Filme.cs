c7cdbff6a4880a794786c36b9a781c1f724adc59
Ighor Rhafael <ighorrhafael@hotmail.com>
2025-01-21 16:33:49 +0000

    Adiciona validações

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -1,9 +1,16 @@
-﻿namespace FilmesAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
 
 public class Filme
 {
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
     public string Titulo { get; set; }
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
     public string Genero { get; set; }
+    [Required(ErrorMessage = "A duração do filme é obrigatório")]
+    [Range(60,600, ErrorMessage = "Duração do filme deve ter entre 60 e 600 minutos")]
     public int Duracao { get; set; }
 
 }
