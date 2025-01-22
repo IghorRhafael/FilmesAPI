31f25de050a0fe91a3bf6ce4aa696ecab13e5742
Ighor Rhafael <ighorrhafael@hotmail.com>
2025-01-22 13:35:44 +0000

    paginação e boas praticas REST

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -8,13 +8,27 @@ namespace FilmesAPI.Controllers;
 public class FilmeController : Controller
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
-    public void AddFilme([FromBody] Filme filme)
+    public IActionResult AddFilme([FromBody] Filme filme)
     {
-        filmes.Add(filme);//adiciona filme
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Genero); 
-        Console.WriteLine(filme.Duracao);
+        filme.Id = id++;
+        filmes.Add(filme);
+        return CreatedAtAction(nameof(GetFilmeByID), new { id = filme.Id}, filme);//retorna o filme criado e o caminho para consulta (Headers / Location)
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> GetFilme([FromQuery] int skip = 0, [FromQuery] int take = 10)
+    {
+        //retorna a lista de filmes dentro de um intervalo para paginação
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetFilmeByID(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        return filme == null ? NotFound() : Ok(filme);//caso filme não exista retorna 404
     }
 }
--- Models/Filme.cs
+++ Models/Filme.cs
@@ -4,6 +4,7 @@ namespace FilmesAPI.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
     [Required(ErrorMessage = "O título do filme é obrigatório")]
     public string Titulo { get; set; }
     [Required(ErrorMessage = "O gênero do filme é obrigatório")]
@@ -12,5 +13,5 @@ public class Filme
     [Required(ErrorMessage = "A duração do filme é obrigatório")]
     [Range(60,600, ErrorMessage = "Duração do filme deve ter entre 60 e 600 minutos")]
     public int Duracao { get; set; }
-
+    
 }
