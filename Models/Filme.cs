0da6829d20ee632e4c62be52de90427c603f6831
Ighor Rhafael <ighorrhafael@hotmail.com>
2025-01-22 13:45:53 +0000

    adição entity

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -6,10 +6,10 @@ public class Filme
 {
     public int Id { get; set; }
     [Required(ErrorMessage = "O título do filme é obrigatório")]
-    public string Titulo { get; set; }
+    public required string Titulo { get; set; }
     [Required(ErrorMessage = "O gênero do filme é obrigatório")]
     [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
-    public string Genero { get; set; }
+    public required string Genero { get; set; }
     [Required(ErrorMessage = "A duração do filme é obrigatório")]
     [Range(60,600, ErrorMessage = "Duração do filme deve ter entre 60 e 600 minutos")]
     public int Duracao { get; set; }
